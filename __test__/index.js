f021b1a38b425a7baf370daed83bad22b48b64d4
zhuminghui2 <zhuminghui2@jd.com>
2024-06-17 16:37:07 +0800

    feat: 修改

--- __test__/index.js
+++ __test__/index.js
@@ -2,9 +2,8 @@ const fs = require('fs')
 const path = require('path')
 process.env.platform = 'arm64'
 const { parse } = require('../index.js')
-const component = fs.readFileSync(path.resolve(__dirname, 'fixure/pesudo.jsx'), 'utf8')
 const css1 = fs.readFileSync(path.resolve(__dirname, 'fixure/pesudo.scss'), 'utf8')
-const { code } = parse(component, [css1], {
+const { code } = parse([css1], {
   platformString: 'Harmony',
   isEnableNesting: true
 })
